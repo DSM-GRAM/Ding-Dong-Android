700c94f9eec5432fa234a2bf112d67800591ca6b
DaeunJeong <alal8272@gmail.com>
2018-05-10 17:15:49 +0900

    StartAcitivity 완료

--- app/src/main/java/com/example/dsm2017/dingdong/StartActivity.kt
+++ app/src/main/java/com/example/dsm2017/dingdong/StartActivity.kt
@@ -1,12 +1,23 @@
 package com.example.dsm2017.dingdong
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        btn_start_login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        btn_start_signup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
