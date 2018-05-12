d8c503243364a150c0dbbe30c9ef70a6c446db5d
DaeunJeong <alal8272@gmail.com>
2018-05-13 01:11:26 +0900

    fix activity_sign_up
       fix edit_signup_cycle
       add text_cycle

--- app/src/main/java/com/example/dsm2017/dingdong/SignUpActivity.kt
+++ app/src/main/java/com/example/dsm2017/dingdong/SignUpActivity.kt
@@ -1,12 +1,32 @@
 package com.example.dsm2017.dingdong
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.graphics.Color.parseColor
+import android.graphics.Typeface
+import android.graphics.Typeface.BOLD
+import android.text.style.ForegroundColorSpan
+import android.text.SpannableStringBuilder
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.StyleSpan
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
+        val cycleString = "평균 월경주기는 25~35일 입니다."
+        val  cycleChangeColor= SpannableStringBuilder(cycleString)
+        cycleChangeColor.setSpan(ForegroundColorSpan(Color.parseColor("#ff717c")), 9, 14, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        cycleChangeColor.setSpan(AbsoluteSizeSpan(60), 9, 14, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        cycleChangeColor.setSpan(StyleSpan(BOLD), 9, 14, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        text_cycle.setText(cycleChangeColor);
     }
 }
