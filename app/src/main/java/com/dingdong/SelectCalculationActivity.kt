9b2d3a022c32e7b3d4987587a7c6f568d2b8a097
DaeunJeong <alal8272@gmail.com>
2018-05-28 20:30:59 +0900

    add activity_select_calculation

---
+++ app/src/main/java/com/dingdong/SelectCalculationActivity.kt
@@ -0,0 +1,12 @@
+package com.dingdong
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectCalculationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_calculation)
+    }
+}
