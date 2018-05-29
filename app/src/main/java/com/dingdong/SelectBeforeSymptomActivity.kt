b8cb2c4b651fd40bbf3082c33fcac1f3e45d0947
DaeunJeong <alal8272@gmail.com>
2018-05-29 23:45:39 +0900

    add activity_select_before_symptom

---
+++ app/src/main/java/com/dingdong/SelectBeforeSymptomActivity.kt
@@ -0,0 +1,12 @@
+package com.dingdong
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectBeforeSymptomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_before_symptom)
+    }
+}
